24c2bfffcbfa38d735fbd0a2edd82a7fb1552e64
Yosef Leibman <yosefleibman@gmail.com>
2020-02-17 17:30:12 -0500

    Login/Signup component init

---
+++ moviegram/src/Components/SignupScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+class SignupScreen extends React.Component {
+    render() {
+      return (
+        <div>hi</div>
+      );
+    }
+  }
+
+  export default SignupScreen;
