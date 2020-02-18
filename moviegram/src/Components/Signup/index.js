53bf2cde104470f11ec07472f8e1435bd9ebdd51
Yosef Leibman <yosefleibman@gmail.com>
2020-02-17 21:55:51 -0500

    signup

--- moviegram/src/Components/Signup/index.js
+++ moviegram/src/Components/Signup/index.js
@@ -1,12 +1,46 @@
 import React from 'react';
-
+import './styles.css';
+import { Button, Form } from 'react-bootstrap';
 
 class SignupScreen extends React.Component {
-    render() {
-      return (
-        <div>Signup</div>
-      );
-    }
+  render() {
+    return (
+      <div className="pageM">
+        <Form className="form">
+          <Form.Group className="welcomeText">
+            <Form.Label>Signup</Form.Label>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Username</Form.Label>
+            <Form.Control type="username" placeholder="Enter username" />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+
+          
+          
+           
+            <Button variant="outline-primary" type="submit" className="loginButton">
+              Signup
+            </Button>
+            
+         
+          
+
+      </Form>
+      </div>
+        
+      
+      
+    );
   }
+}
 
   export default SignupScreen;
