451caaae120b4add82ec65b4ee398efe0da652db
Bhavya Shah <bhavyashah9962@gmail.com>
2020-02-18 22:00:28 -0500

    changed menu bar items from buttons to images

--- moviegram/src/Components/MainMenuBar/index.js
+++ moviegram/src/Components/MainMenuBar/index.js
@@ -9,12 +9,33 @@ class MainMenuBar extends React.Component {
     return (
       <div id="mainMenuBar">
         <ul>
-          <li><Button variant="outline-primary" className="mainMenu-btn" as={Link} to="/login" type="submit">Sign Out</Button></li>
-          <li><Button variant="primary" className="mainMenu-btn" as={Link} to="/" type="submit">Home</Button></li>
-          <li><Button variant="primary" className="mainMenu-btn" as={Link} to="/NewsFeed" type="submit">News Feed</Button></li>
-          <li><Button variant="primary" className="mainMenu-btn" as={Link} to="/" type="submit">Profile</Button></li>
-          <li><Button variant="primary" className="mainMenu-btn" as={Link} to="/" type="submit">Add Review</Button></li>
-          <li><Button variant="outline-primary" className="mainMenu-btn" type="submit" disabled>CurrUsername</Button></li>
+          <li><Button variant="outline-primary" className="edge-btn" as={Link} to="/login" type="submit">Sign Out</Button></li>
+          
+          <li>
+            <a href='/'>
+            <img src="home.png" alt="Home" className="mainMenu-btn"/>
+            </a>
+          </li>
+
+          <li>
+            <a href='/NewsFeed'>
+            <img src="newsFeed.png" alt="News Feed" className="mainMenu-btn"/>
+            </a>
+          </li>
+
+          <li>
+            <a href='/UserProfile'>
+            <img src="profile.png" alt="Profile" className="mainMenu-btn"/>
+            </a>
+          </li>
+
+          <li>
+            <a href='/AddReview'>
+            <img src="addReview.png" alt="Add Review" className="mainMenu-btn"/>
+            </a>
+          </li>
+
+          <li><Button variant="outline-primary" className="edge-btn" type="submit" disabled>CurrUsername</Button></li>
         </ul>
       </div>
     );
