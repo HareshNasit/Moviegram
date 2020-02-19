a26838bdfed21e77dd05d7d779ca7d9b86bfbcfe
Bhavya Shah <bhavyashah9962@gmail.com>
2020-02-19 03:44:02 -0500

    added the review form to the add review page and the movie review spoiler check and a post review button. also added the menu bar to the add review page

--- moviegram/src/Components/AddReview/index.js
+++ moviegram/src/Components/AddReview/index.js
@@ -4,25 +4,45 @@ import { Button, Form } from "react-bootstrap";
 import ReactDOM from 'react-dom';
 import Modal from 'react-modal';
 
+import MainMenuBar from './../MainMenuBar';
+
 class AddReview extends React.Component {
 
   render() {
 
-    const { username, movieName, reviewContent, commentsSection} = this.props;
+    const { username } = this.props;
 
     return (
       <div id="addreviewmain">
+         <MainMenuBar/>
 
          <div className="pageHeader">
             <h3 className="headerText">Add a New Review</h3>
          </div>
-      
+
          <div className="add-review">
             <ul>
               <li>User : {username}</li>
             </ul>
+            <Form className="reviewForm">
+              <Form.Group controlId="review.movieName">
+                <Form.Label>Movie</Form.Label>
+                <Form.Control type="name" placeholder="Movie Name" />
+              </Form.Group>
+              <Form.Group controlId="review.reviewContent">
+                <Form.Label>Review</Form.Label>
+                <Form.Control type="review" as="textarea" rows="8" placeholder="Add your Review here" />
+              </Form.Group>
+              <Form.Group controlId="review.reviewContent">
+                <Form.Check type="checkbox" className="spoilerCheck">
+                  <Form.Check.Input type="checkbox" isValid />
+                  <Form.Check.Label>Spoilers?</Form.Check.Label>
+                </Form.Check>
+              </Form.Group>
+            </Form>
+            <Button variant="primary" className="saveReviewBtn" onClick="" type="submit">Post Review</Button>
          </div>
-      
+
       </div>
     );
   }
--- moviegram/src/Components/NewsFeed/index.js
+++ moviegram/src/Components/NewsFeed/index.js
@@ -10,17 +10,17 @@ class NewsFeedScreen extends React.Component {
 
   state = {
     currUser: "",
-    reviews: [{ username: "Bhavya" , movieName: "Avengers" , 
-                reviewContent: "This movie is lit, and I mean lit af. My god what an awesome time I had and it was even more fun because I watched it with my close friends which made the experience amazing. 15/10 stars. Definitely go watch it pleaseeeeeeeee!!!!!!!!!!!" , 
+    reviews: [{ username: "Bhavya" , movieName: "Avengers" ,
+                reviewContent: "This movie is lit, and I mean lit af. My god what an awesome time I had and it was even more fun because I watched it with my close friends which made the experience amazing. 15/10 stars. Definitely go watch it pleaseeeeeeeee!!!!!!!!!!!" ,
                 commentsSection: "Comments section Component goes here" },
-                { username: "Harsh" , movieName: "Messi Rocks" , 
-                  reviewContent: "I think this movie is better than any movie ever made about any footballer or any movie that will be made about any footballer because Messi by far is the GOAT football player to ever live and anyone who disagrees and says Ronaldo is the best is RIGHT cause messi sucks HaHaHa tricked you there didn't I." , 
+                { username: "Harsh" , movieName: "Messi Rocks" ,
+                  reviewContent: "I think this movie is better than any movie ever made about any footballer or any movie that will be made about any footballer because Messi by far is the GOAT football player to ever live and anyone who disagrees and says Ronaldo is the best is RIGHT cause messi sucks HaHaHa tricked you there didn't I." ,
                   commentsSection: "Comments section Component goes here" },
-                { username: "Dhruv" , movieName: "Dangal" , 
-                  reviewContent: "Ab toh dangal hoga dangal bhenchooodddddd !!!!!!" , 
+                { username: "Dhruv" , movieName: "Dangal" ,
+                  reviewContent: "Ab toh dangal hoga dangal bhenchooodddddd !!!!!!" ,
                   commentsSection: "Comments section Component goes here" },
-                { username: "Yosef" , movieName: "Stalingrad" , 
-                  reviewContent: "Thomas Kretschmann is the best actor I have ever witnessed in my whole damn life and man is he hot." , 
+                { username: "Yosef" , movieName: "Stalingrad" ,
+                  reviewContent: "Thomas Kretschmann is the best actor I have ever witnessed in my whole damn life and man is he hot." ,
                   commentsSection: "Comments section Component goes here" }]
   };
 
@@ -30,7 +30,7 @@ class NewsFeedScreen extends React.Component {
 
         {/*The menu bar is just reused from the Component MainMenuBar */}
         <MainMenuBar/>
-        
+
         <Form className="searchMovieform">
           <Form.Group>
             <Form.Control type="searchMovie" placeholder="Search Movies/Reviews" />
@@ -40,14 +40,14 @@ class NewsFeedScreen extends React.Component {
         <div className="pageHeader">
          <h3 className="headerText">Reviews Feed</h3>
         </div>
-          
+
         {this.state.reviews.map(review => (
           <Review username={review.username}
                   movieName={review.movieName}
                   reviewContent={review.reviewContent}
                   commentsSection={review.commentsSection}/>
         ))}
-  
+
       </div>
     );
   }
