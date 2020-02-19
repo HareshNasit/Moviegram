d6877084d1c614cfe655c56d0044b9476ea5fd00
Bhavya Shah <bhavyashah9962@gmail.com>
2020-02-19 00:59:59 -0500

    added the add review component and the add review route

---
+++ moviegram/src/Components/AddReview/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./styles.css";
+import { Button, Form } from "react-bootstrap";
+
+class AddReview extends React.Component {
+
+  render() {
+
+    return (
+      <div id="add-review">
+        
+      </div>
+    );
+  }
+}
+
+export default AddReview;
--- moviegram/src/Components/MainMenuBar/index.js
+++ moviegram/src/Components/MainMenuBar/index.js
@@ -8,35 +8,35 @@ class MainMenuBar extends React.Component {
   render() {
     return (
       <div id="mainMenuBar">
-        <ul>
-          <li><Button variant="outline-primary" className="edge-btn" as={Link} to="/login" type="submit">Sign Out</Button></li>
-          
-          <li>
-            <a href='/'>
-            <img src="home.png" alt="Home" className="mainMenu-btn"/>
-            </a>
-          </li>
 
-          <li>
-            <a href='/NewsFeed'>
-            <img src="newsFeed.png" alt="News Feed" className="mainMenu-btn"/>
-            </a>
-          </li>
+        <div id="menuItems">
+          <ul>
+            <li>
+              <a href='/'>
+              <img src="home.png" alt="Home" className="mainMenu-btn"/>
+              </a>
+            </li>
 
-          <li>
-            <a href='/UserProfile'>
-            <img src="profile.png" alt="Profile" className="mainMenu-btn"/>
-            </a>
-          </li>
+            <li>
+              <a href='/NewsFeed'>
+              <img src="newsFeed.png" alt="News Feed" className="mainMenu-btn"/>
+              </a>
+            </li>
 
-          <li>
-            <a href='/AddReview'>
-            <img src="addReview.png" alt="Add Review" className="mainMenu-btn"/>
-            </a>
-          </li>
+            <li>
+              <a href='/UserProfile'>
+              <img src="profile.png" alt="Profile" className="mainMenu-btn"/>
+              </a>
+            </li>
 
-          <li><Button variant="outline-primary" className="edge-btn" type="submit" disabled>CurrUsername</Button></li>
-        </ul>
+            <li>
+              <a href='/AddReview'>
+              <img src="addReview.png" alt="Add Review" className="mainMenu-btn"/>
+              </a>
+            </li>
+          </ul>
+        </div>
+
       </div>
     );
   }
