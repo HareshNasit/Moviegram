02319107b7eb6fa4e80adb0296617e0dc2ecc6c9
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2020-02-19 21:37:05 -0500

    Pheww! FInally styled all the components properly :)

--- moviegram/src/Components/UserProfile/index.js
+++ moviegram/src/Components/UserProfile/index.js
@@ -5,28 +5,48 @@ import MainMenuBar from './../MainMenuBar';
 import Image from 'react-bootstrap/Image'
 import photo from './ballon_dor.jpg'
 
+// <MainMenuBar/>
 // <img class="profilePic" src={photo} />
+// <div class="profileDescription">
+//   <h2>Profile Information</h2>
+//   <p class="grey">@movieFreak</p>
+//   <p>
+//       Marvel fan
+//   </p>
+// </div>
+
+// <div class="userProfile">
+//     <div id="profilePicContainer">
+//       CR7
+//     </div>
+//     <div id="profileInfo">
+//       HALA MADRID <br/>
+//       asmdjasbkdjsand
+//     </div>
+// </div>
+
 class UserProfile extends React.Component {
 
   render() {
     return (
-      <div class="userProfile">
-        <MainMenuBar/>
-        <div class="bodyHeader">
-          <div id="profilePicContainer">
-            <img class="profilePic" src={photo} />
+      <div id="userProfile">
+          <MainMenuBar/>
+          <div id="bodyHeader">
+            <div id="profilePicContainer">
+              <img class="profilePic" src={photo} />
+            </div>
+            <div id="profileInfo">
+              HALA MADRID <br/>
+              asmdjasbkdjsand
+            </div>
           </div>
-          <div id="profileInfo">
-            HALA MADRID
+          <div className="profileDescription">
+            <h2>Profile Information</h2>
+            <p>@movieFreak</p>
+            <p>
+                Marvel fan
+            </p>
           </div>
-        </div>
-        <div class="profileDescription">
-          <h2>Profile Information</h2>
-          <p class="grey">@movieFreak</p>
-          <p>
-              Marvel fan
-          </p>
-        </div>
       </div>
     );
   }
