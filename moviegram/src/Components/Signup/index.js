76dfbcae4161e8ffe627488d5a9d37e3f69137e0
Yosef Leibman <yosefleibman@gmail.com>
2020-02-21 17:54:17 -0500

    changed div class

--- moviegram/src/Components/Signup/index.js
+++ moviegram/src/Components/Signup/index.js
@@ -6,7 +6,7 @@ import {Link} from 'react-router-dom';
 class SignupScreen extends React.Component {
   render() {
     return (
-      <div className="pageM">
+      <div className="formContainer">
         <Form className="form">
           <Form.Group className="welcomeText">
             <Form.Label>Signup</Form.Label>
