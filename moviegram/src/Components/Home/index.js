fa5aa41ede9a7334790ecda921089dd05e686fc0
Dhruv Patel <dhruvp68@gmail.com>
2020-02-28 17:40:37 -0500

    CSS Changes

--- moviegram/src/Components/Home/index.js
+++ moviegram/src/Components/Home/index.js
@@ -2,24 +2,52 @@ import React from 'react';
 import './styles.css'
 import { Button, Form } from "react-bootstrap";
 import {Link} from 'react-router-dom';
+import ReactSearchBox from 'react-search-box'
 
 
 
 
 class Home extends React.Component {
+  data = [
+    {
+      key: 'john',
+      value: 'John Doe',
+    },
+    {
+      key: 'jane',
+      value: 'Jane Doe',
+    },
+    {
+      key: 'mary',
+      value: 'Mary Phillips',
+    },
+    {
+      key: 'robert',
+      value: 'Robert',
+    },
+    {
+      key: 'karius',
+      value: 'Karius',
+    },
+  ]
     render() {
       return (
-      	<div className="Background center">
+        <div className="Home">
+      	  <div className="Background center">
+          </div>
+
 
         	<div className="Banner">
       			<img src={require("./static/Banner.jpg")} />
       		</div>
 
-      		<Form className="searchMovie">
-          	  <Form.Group>
-            	<Form.Control type="searchMovie" placeholder="Search Movie" />
-              </Form.Group>
-        	</Form>
+      		<div className="searchMovie">
+            <ReactSearchBox
+            placeholder="Search Movie"
+            data={this.data}
+            onSelect={record => console.log(record)}
+            />
+        	</div>
 
           <div className="searchButton">
               <Button type="submit" variant="outline-primary">Search</Button>
@@ -29,8 +57,9 @@ class Home extends React.Component {
           </div>
 
 
-      	</div>
-          	     	
+
+        </div>
+
       );
     }
   }
