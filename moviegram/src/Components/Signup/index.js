e10658dd9dc85e94160b81930bc22feb8b25c9e4
Yosef Leibman <yosefleibman@gmail.com>
2020-03-02 22:48:15 -0500

    Add Genres Browse Image added

--- moviegram/src/Components/Signup/index.js
+++ moviegram/src/Components/Signup/index.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import './styles.css';
-import { Button, Form } from 'react-bootstrap';
+import { Button, Form, Modal } from 'react-bootstrap';
 import {Link, Redirect} from 'react-router-dom';
+import Dialog from 'react-bootstrap-dialog';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+
 
 const constants = require("../../constants")
 console.log(constants)
 class SignupScreen extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {email: "", password: ""};
+    this.genreNames = ["Supernatural", 
+                       "Fantasy", 
+                       "Crime", 
+                       "Action",
+                       "Horror",
+                       "Thriller",
+                       "Comedy"]
+    this.state = {email: "", password: "", genresShow: false, genres: 
+            {Supernatural: false, 
+              Horror: false, 
+              Fantasy: false, 
+              Crime: false,
+              Action: false,
+              Thriller: false,
+              Comedy: false
+    }};
     this.changeEmail = this.changeEmail.bind(this);
     this.buttonClick = this.buttonClick.bind(this);
     this.passwordChange = this.passwordChange.bind(this)
+
   }
 
   changeEmail(e) {
@@ -19,30 +40,36 @@ class SignupScreen extends React.Component {
     console.log(this.state.email);
  }
 
- passwordChange(e) {
-    this.setState({password: e.target.value});
- }
-
- buttonClick(){
-  constants.users.push({user: {email: this.state.email,
-                              password: this.state.password,
-                              username:this.state.username}});
-  this.props.history.push("/login")
- }
-
- renderRedirect = () => {
-  if (constants.acc.auth) {
-    return <Redirect to='/NewsFeed' />
+  passwordChange(e) {
+      this.setState({password: e.target.value});
   }
-}
+
+  buttonClick(){
+    constants.users.push({user: {email: this.state.email,
+                                password: this.state.password,
+                                username:this.state.username}});
+    if(this.state.email != "" 
+          && this.state.password != "" 
+          && this.state.username != ""){
+            this.props.history.push("/login")
+    }
+  }
+
+  renderRedirect = () => {
+    if (constants.acc.auth) {
+      return <Redirect to='/NewsFeed' />
+    }
+  }
+
+
   render() {
     return (
       <div className="formContainer">
         {this.renderRedirect()}
-
+     
         <Form className="form">
           <Form.Group className="welcomeText">
-            <Form.Label>Signup</Form.Label>
+            <Form.Label className="centerText">Signup</Form.Label>
           </Form.Group>
           <Form.Group>
             <Form.Label>Username</Form.Label>
@@ -58,6 +85,19 @@ class SignupScreen extends React.Component {
             <Form.Control type="password" placeholder="Password" onChange={this.passwordChange} />
           </Form.Group>
 
+          <Button variant="outline-primary"
+                    className="addGenreButton"
+                    onClick={() => this.setState({genresShow: true})}
+                    >
+                    Add Genres
+                  </Button>
+          <Form.Group>
+            <Form.Label className="selectImage">Select your profile picture</Form.Label>
+            <input
+              accept="image/*"
+              type="file"
+            />
+          </Form.Group>
 
             <Button variant="outline-primary"
                             onClick={this.buttonClick}
@@ -69,8 +109,42 @@ class SignupScreen extends React.Component {
             </div>
 
 
-
       </Form>
+
+      <Modal
+        show={this.state.genresShow}
+        onHide={() => this.setState({genresShow: false})}
+        dialogClassName="modal-90w"
+        aria-labelledby="example-custom-modal-styling-title"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="example-custom-modal-styling-title">
+            Select your favourite Genres
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <FormGroup className="checkBoxesContainer">
+              {this.genreNames.map( (obj) => {
+                 return(<FormControlLabel
+                 control={
+                   <Checkbox 
+                             checked={this.state.genres[obj]} 
+                             onChange={() => this.setState(prevState => {
+                               let genres = Object.assign({}, prevState.genres);  
+                               genres[obj] = !genres[obj]                  
+                               return { genres };                                 
+                             })} 
+                             value="Horror" />
+                           
+                 }
+                 label={obj}
+               />);
+              }
+              )}
+             
+          </FormGroup>
+        </Modal.Body>
+      </Modal>
       </div>
 
 
