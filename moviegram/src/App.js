fee8a965a4232b5ef48dfb4cddc0971887584b08
Yosef Leibman <yosefleibman@gmail.com>
2020-03-02 21:04:52 -0500

    fixed master

--- moviegram/src/App.js
+++ moviegram/src/App.js
@@ -13,6 +13,7 @@ import SignupScreen from './Components/Signup';
 import NewsFeedScreen from './Components/NewsFeed'
 import AddReview from './Components/AddReview'
 import UserProfile from './Components/UserProfile'
+import Profile from './Components/Profile'
 import Movie from './Components/Movie'
 
 
@@ -37,22 +38,24 @@ class App extends React.Component {
                             (<NewsFeedScreen {...props} />)}/>
              <Route exact path='/AddReview' render={(props) =>
                             (<AddReview {...props} />)}/>
+            <Route exact path='/profile/:id' render={(props) =>
+                            (<Profile {...props} />)}/>
             <Route exact path='/userProfile' render={(props) =>
                             (<UserProfile {...props} />)}/>
             <Route exact path='/movie/dangal' render={(props) =>
-                            (<Movie title="Dangal" 
+                            (<Movie description="This is a movie" title="Dangal"
                             imgsrc="https://files.delhievents.com/images/events/2016/december/Dangal-movie-poster.jpg" {...props} />)}/>
             <Route exact path='/movie/avengers: endgame' render={(props) =>
-                            (<Movie title="Avengers: Endgame"
+                            (<Movie description="This is a movie" title="Avengers: Endgame"
                             imgsrc="https://upload.wikimedia.org/wikipedia/en/0/0d/Avengers_Endgame_poster.jpg" {...props} />)}/>
             <Route exact path='/movie/mission impossible 5' render={(props) =>
-                            (<Movie title="Mission Impossible 5"
+                            (<Movie description="This is a movie" title="Mission Impossible 5"
                             imgsrc="https://image.tmdb.org/t/p/original/z2sJd1OvAGZLxgjBdSnQoLCfn3M.jpg" {...props} />)}/>
             <Route exact path='/movie/interstellar' render={(props) =>
-                            (<Movie title="Interstellar"
+                            (<Movie description="This is a movie" title="Interstellar"
                             imgsrc="https://mir-s3-cdn-cf.behance.net/project_modules/2800_opt_1/b0f70f29979965.560d298403bc7.jpg" {...props} />)}/>
             <Route exact path='/movie/fate of the furious' render={(props) =>
-                            (<Movie title="Fate of the Furious"
+                            (<Movie description="This is a movie" title="Fate of the Furious"
                             imgsrc="https://m.media-amazon.com/images/M/MV5BMjMxODI2NDM5Nl5BMl5BanBnXkFtZTgwNjgzOTk1MTI@._V1_SY1000_CR0,0,631,1000_AL_.jpg" {...props} />)}/>
           </Switch>
         </BrowserRouter>
