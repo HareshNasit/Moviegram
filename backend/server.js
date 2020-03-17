462075b185d8310f6afca3d57d5c388c96406daa
Yosef Leibman <yosefleibman@gmail.com>
2020-03-17 10:53:13 -0400

    models and api setup

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+/* server.js, with mongodb API and static directories */
+'use strict';
+const log = console.log
+const path = require('path')
+
+const express = require('express')
+// starting the express server
+const app = express();
+
+// mongoose and mongo connection
+const { mongoose } = require('./db/mongoose')
+
+// import the mongoose models
+// models
+
+// to validate object IDs
+const { ObjectID } = require('mongodb')
+
+// body-parser: middleware for parsing HTTP JSON body into a usable object
+const bodyParser = require('body-parser') 
+app.use(bodyParser.json())
+
+
+/*********************************************************/
+
+/*** API Routes below ************************************/
+
+app.get('/user/:id', (req, res) => {
+	
+})
+
+
+
+/*************************************************/
+// Express server listening...
+const port = process.env.PORT || 5000
