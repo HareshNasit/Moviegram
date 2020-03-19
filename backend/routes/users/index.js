21d9718551d21f0dde32cd8dd87bb5968173694b
Yosef Leibman <yosefleibman@gmail.com>
2020-03-18 21:15:32 -0400

    Route conventions update for users

--- backend/routes/users/index.js
+++ backend/routes/users/index.js
@@ -1,8 +1,8 @@
 const app = module.exports = require('express')();
 
 /****** User routes ************************************ */
-app.get('/user/:id', require("./get_user"))
-app.get('/all_users', require("./all_users"))
+app.get('/:id', require("./get_user"))
+app.get('/', require("./all_users"))
 
-app.post('/add_user', require("./add_user"))
+app.post('/', require("./add_user"))
 /*******************************************************/
