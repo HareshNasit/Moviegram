bce46d6999b7fc883b264eb5d7e4d29e12ce4879
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2020-03-19 00:17:44 -0400

    Added code for adding user in backend

--- backend/routes/users/add_user.js
+++ backend/routes/users/add_user.js
@@ -1,4 +1,28 @@
+const { User } = require('./../../models/user')
 
 module.exports = async (req, res) => {
-    res.send("Add a User")
-  }
+    res.send('Yalla');
+}
+
+// const user = req.body
+// if(!user){
+//   res
+//     .status(400)
+//     .send('Invalid User specified.')
+//     .end();
+// }
+// User.insertOne({
+// //_id: 7,  // can set your own id or use default
+//   username: user.username,
+//   email: user.email,
+//   password: user.password,
+//   favoriteGenres: user.favoriteGenres
+//
+// }, (error, result) => {
+//   if (error) {
+//     log("Can't insert user", error)
+//   } else {
+//     log(result.ops)
+//     log(result.ops[0]._id.getTimestamp())
+//   }
+// })
--- backend/routes/users/get_user.js
+++ backend/routes/users/get_user.js
@@ -3,7 +3,7 @@ const { User } = require('./../../models/user')
 
 module.exports = async (req, res) => {
     User.findOne({username: req.params.id}).then((user) => {
-      res.send(user) 
+      res.send(user)
     }, (error) => {
       res.status(500).send(error) // server error
     })
