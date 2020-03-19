d8bcfba6d1d8deca24d954823a3bb2264f382fad
Yosef Leibman <yosefleibman@gmail.com>
2020-03-18 21:12:32 -0400

    key pairings query

--- backend/routes/movies/index.js
+++ backend/routes/movies/index.js
@@ -4,6 +4,6 @@ const app = module.exports = require('express')();
 /****** Movie routes *************************************/
 app.get('/', require("./all_movies"))
 app.get('/:id', require("./get_movie"))
-
+app.get('/keypairs', require("./title_key_pairings"))
 
 /*******************************************************/
