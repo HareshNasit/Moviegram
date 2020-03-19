d0060b7379bf6fb2149e1495cb41ab20e6033149
Yosef Leibman <yosefleibman@gmail.com>
2020-03-18 21:06:25 -0400

    Added add_movie and change status codes for all_movies and get_movie

--- backend/routes/movies/add_movie.js
+++ backend/routes/movies/add_movie.js
@@ -1,4 +1,16 @@
+const { Movies } = require('./../../models/movie')
+
 
 module.exports = async (req, res) => {
-    res.send("Add a Movie")
-  }
+    Movies.insert({
+         "_id": req.body.id,
+         "title": req.body.title,
+         "Director": req.body.director,
+         "stars": req.body.actors,
+         "description": req.body.description
+       }).then(() => {
+        res.status(200).send()
+      }, (error) => {
+        res.status(500).send(error) // server error
+      })
+    }
--- backend/routes/movies/all_movies.js
+++ backend/routes/movies/all_movies.js
@@ -5,6 +5,6 @@ module.exports = async (req, res) => {
     Movies.find().then((movies) => {
       res.send(movies) // can wrap in object if want to add more properties
     }, (error) => {
-      res.status(500).send(error) // server error
+      res.status(404).send(error) // server error
     })
   }
