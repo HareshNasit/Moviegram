592f2443cb880d83026bdacabed43dee407e0d4e
Yosef Leibman <yosefleibman@gmail.com>
2020-03-18 20:19:56 -0400

    getting movie by id

--- backend/routes/movies/get_movie.js
+++ backend/routes/movies/get_movie.js
@@ -2,7 +2,7 @@
 const { Movies } = require('./../../models/movie')
 
 module.exports = async (req, res) => {
-  Movies.findOne({title: req.params.id}).then((movies) => {
+  Movies.findOne({_id: req.params.id}).then((movies) => {
 		res.send({ movies }) // can wrap in object if want to add more properties
 	}, (error) => {
 		res.status(500).send(error) // server error
