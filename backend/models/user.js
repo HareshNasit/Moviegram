8d9b7c31f9a94383b85c4673a3e858497292f64c
Yosef Leibman <yosefleibman@gmail.com>
2020-03-20 14:59:20 -0400

    login and changed user schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,7 +1,7 @@
 /* Student mongoose model */
 const mongoose = require('mongoose')
 
-const User = mongoose.model('User', {
+const UserSchema = mongoose.Schema({
 	_id: {
 		type: String,
 		required: true,
@@ -42,5 +42,30 @@ const User = mongoose.model('User', {
 		default: false
 	}
 })
+
+// A static method on the document model.
+// Allows us to find a User document by comparing the hashed password
+//  to a given one, for example when logging in.
+UserSchema.statics.getUserByUsernamePassword = function(username, password) {
+	const User = this // binds this to the User model
+
+	// First find the user by their email
+	return User.findOne({ _id: username }).then((user) => {
+		if (!user) {
+			return Promise.reject()  // a rejected promise
+		}
+		// if the user exists, make sure their password is correct
+		return new Promise((resolve, reject) => {
+			bcrypt.compare(password, user.password, (err, result) => {
+				if (result) {
+					resolve(user)
+				} else {
+					reject()
+				}
+			})
+		})
+	})
+}
+const User = mongoose.model('User', UserSchema)
 
 module.exports = { User }
--- backend/routes/login/login.js
+++ backend/routes/login/login.js
@@ -1,9 +1,24 @@
 const { User } = require('./../../models/user')
 const bcrypt = require('bcrypt');
 
+
 module.exports = async (req, res) => {
     /**** STILL NEED TO IMPLEMENT */
-    bcrypt.compare(myPlaintextPassword, hash, function(err, result) {
-      // result == true
-    });
+    const username = req.body.username
+    const password = req.body.password
+
+    // Use the static method on the User model to find a user
+    // by their email and password
+    User.getUserByUsernamePassword(username, password).then((user) => {
+	    if (!user) {
+            res.redirect('/login');
+        } else {
+            // Add the user's id to the session cookie.
+            // We can check later if this exists to ensure we are logged in.
+            req.session.user = user._id;
+            res.redirect('/newsfeed');
+        }
+    }).catch((error) => {
+		  res.status(400).redirect('/login');
+    })
   }
