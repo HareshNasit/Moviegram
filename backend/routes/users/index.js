a3a7f31006c2952d7ddf72190b82f28cc7c3c7dc
Yosef Leibman <yosefleibman@gmail.com>
2020-03-20 22:48:59 -0400

    removed add_user from index

--- backend/routes/users/index.js
+++ backend/routes/users/index.js
@@ -3,6 +3,4 @@ const app = module.exports = require('express')();
 /****** User routes ************************************ */
 app.get('/:id', require("./get_user"))
 app.get('/', require("./all_users"))
-
-app.post('/', require("./add_user"))
 /*******************************************************/
