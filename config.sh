460e6b7ca7def0c01309385ba4fb35a7a004c5b6
Yosef Leibman <yosefleibman@gmail.com>
2020-03-21 17:36:53 -0400

    Commenting config shell script

--- config.sh
+++ config.sh
@@ -1,10 +1,15 @@
 NAME1=moviegram-front
 NAME2=moviegram-back
+# SETUP
 # heroku buildpacks:set https://github.com/heroku/heroku-buildpack-nodejs#v83 -a $NAME1
 # heroku buildpacks:set https://github.com/heroku/heroku-buildpack-nodejs#v83 -a $NAME2
 # heroku buildpacks:add -a $NAME1 https://github.com/heroku/heroku-buildpack-multi-procfile
 # heroku buildpacks:add -a $NAME2 https://github.com/heroku/heroku-buildpack-multi-procfile
+
+# Setting the proc files
 # heroku config:set -a $NAME1 PROCFILE=frontend/Procfile
 # heroku config:set -a $NAME2 PROCFILE=backend/Procfile
+
+# DEPLOYMENT COMMANDS
 git push https://git.heroku.com/$NAME1.git master
-# git push https://git.heroku.com/$NAME2.git master
+git push https://git.heroku.com/$NAME2.git master
