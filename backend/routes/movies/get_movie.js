47b67bcad43a7a325b5f158b704e03a299673908
Yosef Leibman <yosefleibman@gmail.com>
2020-03-25 12:40:37 -0400

    get movie route

--- backend/routes/movies/get_movie.js
+++ backend/routes/movies/get_movie.js
@@ -3,7 +3,7 @@ const { Movies } = require('./../../models/movie')
 
 module.exports = async (req, res) => {
   Movies.findOne({_id: req.params.id}).then((movies) => {
-		res.send({ movies }) // can wrap in object if want to add more properties
+		res.send(movies) // can wrap in object if want to add more properties
 	}, (error) => {
 		res.status(404).send(error) // server error
 	})
