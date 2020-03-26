b37ed74029727c0ac1ffe6394ff028562f31f489
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2020-03-26 16:09:45 -0400

    Added code for get friends review

--- backend/routes/reviews/index.js
+++ backend/routes/reviews/index.js
@@ -4,7 +4,7 @@ const app = module.exports = require('express')();
 /****** Review routes *************************************/
 
 app.get('/', require("./all_reviews"))
-app.get('/reviews/:id', require("./get_friends_reviews"))
+// app.get('/reviews/:id', require("./get_friends_reviews"))
 app.get('/:id', (req, res) => {
 
 })
