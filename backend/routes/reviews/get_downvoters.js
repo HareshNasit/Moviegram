3e999bab1bbc08707056f5f08de9b8724d8b456c
Bhavya Shah <bhavyashah9962@gmail.com>
2020-03-27 02:25:22 -0400

    added get downvoters and upvoters routes

--- backend/routes/reviews/get_downvoters.js
+++ backend/routes/reviews/get_downvoters.js
@@ -2,7 +2,7 @@
 const { Reviews } = require('./../../models/review')
 
 module.exports = async (req, res) => {
-  Reviews.findOne({username: req.params.username, movie_title: req.params.title, date: req.params.date}).then((review) => {
+  Reviews.findOne({"username": req.params.username}, {"movie_title": req.params.title}, {"date": req.params.date}).then((review) => {
 		res.send(review)
 	}, (error) => {
 		res.status(404).send(error) // server error
