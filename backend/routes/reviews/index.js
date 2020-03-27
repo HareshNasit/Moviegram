396fcc38878e5266211996106d27fe5c8443ee26
Bhavya Shah <bhavyashah9962@gmail.com>
2020-03-27 00:58:15 -0400

    changed upvoter downvoter route

--- backend/routes/reviews/index.js
+++ backend/routes/reviews/index.js
@@ -5,8 +5,8 @@ const app = module.exports = require('express')();
 
 app.get('/', require("./all_reviews"))
 app.get('/user_reviews/:username', require("./get_user_reviews"))
-app.get('/:title/:username/:date', require("./get_upvoters"))
-app.get('/:title/:username/:date', require("./get_downvoters"))
+app.get('/get_downvoters/:title/:username/:date', require("./get_upvoters"))
+app.get('/get_upvoters/:title/:username/:date', require("./get_downvoters"))
 // app.get('/:id', require("./get_friends_reviews"))
 app.get('/:id', (req, res) => {
 
