9c6637a9f340ae893b7d86867f65c932cc8b391c
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2020-03-27 16:10:54 -0400

    Merged my changes

---
+++ backend/routes/reviews/get_friends_reviews.js
@@ -0,0 +1,10 @@
+
+const { Reviews } = require('./../../models/review')
+
+module.exports = async (req, res) => {
+  Reviews.find({username: req.params.id}).then((reviews) => {
+		res.send({ reviews }) // can wrap in object if want to add more properties
+	}, (error) => {
+		res.status(404).send(error) // server error
+	})
+}
---
+++ backend/server.js
@@ -0,0 +1,96 @@
+/* server.js, with mongodb API and static directories */
+'use strict';
+const log = console.log
+const path = require('path')
+
+const express = require('express')
+// starting the express server
+const app = express();
+
+// mongoose and mongo connection
+const mongoose = require('mongoose')
+
+// import the mongoose models
+// models
+
+// to validate object IDs
+const { ObjectID } = require('mongodb')
+
+// body-parser: middleware for parsing HTTP JSON body into a usable object
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+const cors = require('cors')
+app.use(cors());
+
+require('dotenv').config();
+
+
+/***********************************************/
+const uri = `mongodb+srv://harsh:${process.env.dbpass}@cluster0-mujm7.mongodb.net/moviegram?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true`;
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true}).catch(
+	error => console.log(error)
+);
+/***********************************************/
+
+// express-session for managing user sessions
+const session = require('express-session')
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/*** Session handling **************************************/
+// Create a session cookie
+app.use(session({
+    secret: 'moviesarefun',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        expires: 60000,
+        httpOnly: true
+    }
+}));
+
+// Our own express middleware to check for
+// an active user on the session cookie (indicating a logged in user.)
+const sessionChecker = (req, res, next) => {
+    if (req.session.user) {
+        res.redirect('/dashboard'); // redirect to dashboard if logged in.
+    } else {
+        next(); // next() moves on to the route.
+    }
+};
+
+// Middleware for authentication of resources
+const authenticate = (req, res, next) => {
+	if (req.session.user) {
+		User.findById(req.session.user).then((user) => {
+			if (!user) {
+				return Promise.reject()
+			} else {
+				req.user = user
+				next()
+			}
+		}).catch((error) => {
+			res.status(401).send("Unauthorized")
+		})
+	} else {
+		res.status(401).send("Unauthorized")
+	}
+}
+
+/*********************************************************/
+
+/*** API Routes below ************************************/
+
+app.use('/session', require('./routes/session'))
+app.use('/users', require('./routes/users'))
+app.use('/movies', require('./routes/movies'))
+app.use('/reviews', require('./routes/reviews'))
+app.use('/signup', require('./routes/signup'))
+app.use('/login', require('./routes/login'))
+
+
+/*************************************************/
+// Express server listening...
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+	log(`Listening on port ${port}...`)
+})
