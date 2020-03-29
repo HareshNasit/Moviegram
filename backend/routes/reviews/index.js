fa559d2d4652ff8334c45b1698b0347c6acdfb22
Bhavya Shah <bhavyashah9962@gmail.com>
2020-03-28 23:43:51 -0400

    added routes to add and delete upvoters and downvoters from a review

--- backend/routes/reviews/index.js
+++ backend/routes/reviews/index.js
@@ -8,13 +8,10 @@ app.get('/user_reviews/:username', require("./get_user_reviews"))
 app.get('/downvoters/:id', require("./get_downvoters"))
 app.get('/upvoters/:id', require("./get_upvoters"))
 
+// Routes for upvoting and downvoting
 app.delete('/delete_upvoter/:id/:upvoter', require('./deleteUpvoter'))
 app.delete('/delete_downvoter/:id/:downvoter', require('./deleteDownvoter'))
+app.patch('/add_upvoter/:id/:upvoter', require('./addUpvoter'))
+app.patch('/add_downvoter/:id/:downvoter', require('./addDownvoter'))
 
-app.get('/:id', (req, res) => {
-
-})
-app.post('/:id', (req, res) => {
-
-})
 /*******************************************************/
