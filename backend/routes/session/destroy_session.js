76867148921d2fc1d9b961dca9a8e97b65b4ed9d
Yosef Leibman <yosefleibman@gmail.com>
2020-04-02 21:30:51 -0400

    logout destroy session

---
+++ backend/routes/session/destroy_session.js
@@ -0,0 +1,10 @@
+module.exports = async (req, res) => {
+	// Remove the session
+	req.session.destroy((error) => {
+		if (error) {
+			res.status(500).send(error)
+		} else {
+			res.redirect('/')
+		}
+	})
+};
