ab1b2255b2338fc5ecf91b52185f1083d3dbbd91
Yosef Leibman <yosefleibman@gmail.com>
2020-04-03 18:08:25 -0400

    no longer session checking all reviews

--- backend/routes/admin/index.js
+++ backend/routes/admin/index.js
@@ -2,7 +2,7 @@ const app = module.exports = require('express')();
 const {sessionCheckerAdmin} = require("../../auth")
 
 /****** Admin routes *************************************/
-app.get('/', sessionCheckerAdmin, require("./all_reviews"))
+app.get('/', require("./all_reviews"))
 app.delete('/remove_review/', sessionCheckerAdmin, require("./remove_review"))
 app.post('/add_movie', sessionCheckerAdmin, require("./../movies/add_movie"))
 
