24cc8e9c09adbb16528f975d6cb4aa3751f09b0e
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2020-04-03 17:02:14 -0400

    Editing genres feature complete

--- backend/routes/users/index.js
+++ backend/routes/users/index.js
@@ -7,4 +7,5 @@ app.get('/get_friends/:id', require("./get_user_following"))
 app.put('/user_update_follow/:id', require('./update_user_follow'))
 app.get('/get_followers/:id', require("./get_user_followers"))
 app.put('/user_update_description/:id', require('./update_description'))
+app.put('/user_update_favoriteGenres/:id', require('./update_favoriteGenres'))
 /*******************************************************/
