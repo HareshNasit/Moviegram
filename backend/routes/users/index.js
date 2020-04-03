e037c555ae57505cca779958b0977c520e35bfa4
Yosef Leibman <yosefleibman@gmail.com>
2020-04-03 17:39:58 -0400

    Sessionchecker back

--- backend/routes/users/index.js
+++ backend/routes/users/index.js
@@ -1,10 +1,13 @@
 const app = module.exports = require('express')();
+const {sessionCheckerReqUserBody, sessionCheckerReqUserParam} = require("../../auth")
+
+
 
 /****** User routes ************************************ */
 app.get('/:id', require("./get_user"))
 app.get('/', require("./all_users"))
 app.get('/get_friends/:id', require("./get_user_following"))
-app.put('/user_update_follow/:id', require('./update_user_follow'))
+app.put('/user_update_follow/', sessionCheckerReqUserBody, require('./update_user_follow'))
 app.get('/get_followers/:id', require("./get_user_followers"))
-app.put('/user_update_description/:id', require('./update_description'))
+app.put('/user_update_description/:id',sessionCheckerReqUserBody, require('./update_description'))
 /*******************************************************/
