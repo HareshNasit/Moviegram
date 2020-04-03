db31421111712b98ae67e84dc82f64b41becf34b
Yosef Leibman <yosefleibman@gmail.com>
2020-04-02 21:31:40 -0400

    remove auth from server.js

--- server.js
+++ server.js
@@ -48,34 +48,6 @@ app.use(session({
     }
 }));
 
-// Our own express middleware to check for
-// an active user on the session cookie (indicating a logged in user.)
-const sessionChecker = (req, res, next) => {
-    if (req.session.user) {
-        res.redirect('/dashboard'); // redirect to dashboard if logged in.
-    } else {
-        next(); // next() moves on to the route.
-    }
-};
-
-// Middleware for authentication of resources
-const authenticate = (req, res, next) => {
-	if (req.session.user) {
-		User.findById(req.session.user).then((user) => {
-			if (!user) {
-				return Promise.reject()
-			} else {
-				req.user = user
-				next()
-			}
-		}).catch((error) => {
-			res.status(401).send("Unauthorized")
-		})
-	} else {
-		res.status(401).send("Unauthorized")
-	}
-}
-
 /*********************************************************/
 
 /*** API Routes below ************************************/
